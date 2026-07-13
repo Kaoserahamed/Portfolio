88727076b5ac54206ddf78d7adcf3451a3f3ccec
kaoser <akaoser41@gmail.com>
2026-07-13 08:42:23 +0600

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Education from './components/Education';
 import Certificates from './components/Certificates';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
